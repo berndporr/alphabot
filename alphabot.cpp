399c035f261f75c1abbe633d7a5c1932b27e2ab1
Bernd Porr <mail@berndporr.me.uk>
2026-01-26 12:02:31 +0000

    Works now with the new gpio v2 c++ library.

--- alphabot.cpp
+++ alphabot.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <unistd.h>
 
-void AlphaBot::start(long _samplingInterval) {
-    left_wheel_pwm.start(2,pwmfrequ);
-    right_wheel_pwm.start(3,pwmfrequ);
+void AlphaBot::start(long _samplingInterval)
+{
+    left_wheel_pwm.start(2, pwmfrequ);
+    right_wheel_pwm.start(3, pwmfrequ);
 
     // ADC
     GPIO_ADC_CS.set_value(1);
@@ -15,41 +16,47 @@ void AlphaBot::start(long _samplingInterval) {
     startms(_samplingInterval);
 }
 
-void AlphaBot::stop() {
+void AlphaBot::stop()
+{
     CppTimer::stop();
-    if ( (leftWheelSpeed > 0) || (rightWheelSpeed > 0) ) {
-	for(int i = 0; i<10; i++) {
-	    setLeftWheelSpeed(leftWheelSpeed/1.5);
-	    setRightWheelSpeed(rightWheelSpeed/1.5);
-	    usleep(100000);
-	}
-	setLeftWheelSpeed(0);
-	setRightWheelSpeed(0);
+    if ((leftWheelSpeed > 0) || (rightWheelSpeed > 0))
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            setLeftWheelSpeed(leftWheelSpeed / 1.5);
+            setRightWheelSpeed(rightWheelSpeed / 1.5);
+            usleep(100000);
+        }
+        setLeftWheelSpeed(0);
+        setRightWheelSpeed(0);
     }
 }
 
-unsigned AlphaBot::readADC(unsigned channel) {
+unsigned AlphaBot::readADC(unsigned channel)
+{
     const unsigned us = 2;
     GPIO_ADC_CS.set_value(0);
     usleep(us);
     channel = channel << 4;
-    for (unsigned i = 0; i < 4; i ++) {
-	if(channel & 0x80)
-	    GPIO_ADC_ADDR.set_value(1);
-	else
-	    GPIO_ADC_ADDR.set_value(0);
-	usleep(us);
-	GPIO_ADC_IOCLK.set_value(1);
-	usleep(us);
-	GPIO_ADC_IOCLK.set_value(0);
-	usleep(us);
-	channel = channel << 1;
+    for (unsigned i = 0; i < 4; i++)
+    {
+        if (channel & 0x80)
+            GPIO_ADC_ADDR.set_value(1);
+        else
+            GPIO_ADC_ADDR.set_value(0);
+        usleep(us);
+        GPIO_ADC_IOCLK.set_value(1);
+        usleep(us);
+        GPIO_ADC_IOCLK.set_value(0);
+        usleep(us);
+        channel = channel << 1;
     }
-    for (unsigned i = 0; i < 6;i ++) {
-	usleep(us);
-	GPIO_ADC_IOCLK.set_value(1);
-	usleep(us);
-	GPIO_ADC_IOCLK.set_value(0);
+    for (unsigned i = 0; i < 6; i++)
+    {
+        usleep(us);
+        GPIO_ADC_IOCLK.set_value(1);
+        usleep(us);
+        GPIO_ADC_IOCLK.set_value(0);
     }
 
     GPIO_ADC_CS.set_value(1);
@@ -58,54 +65,59 @@ unsigned AlphaBot::readADC(unsigned channel) {
 
     unsigned value = 0;
     unsigned mask = 1 << 9;
-    for (unsigned i = 0; i < 2; i ++) {
-	GPIO_ADC_IOCLK.set_value(1);
-	usleep(us);
-	usleep(us);
-	if (GPIO_ADC_DOUT.get_value())
-	    value |= mask;
-	mask >>= 1;
-	GPIO_ADC_IOCLK.set_value(0);
-	usleep(us);
+    for (unsigned i = 0; i < 2; i++)
+    {
+        GPIO_ADC_IOCLK.set_value(1);
+        usleep(us);
+        usleep(us);
+        if (GPIO_ADC_DOUT.get_value())
+            value |= mask;
+        mask >>= 1;
+        GPIO_ADC_IOCLK.set_value(0);
+        usleep(us);
     }
 
-    for (unsigned i = 0; i < 8; i ++) {
-	GPIO_ADC_IOCLK.set_value(1);
-	usleep(us);
-	if (GPIO_ADC_DOUT.get_value())
-	    value |= mask;
-	mask >>= 1;
-	GPIO_ADC_IOCLK.set_value(0);
-	usleep(us);
+    for (unsigned i = 0; i < 8; i++)
+    {
+        GPIO_ADC_IOCLK.set_value(1);
+        usleep(us);
+        if (GPIO_ADC_DOUT.get_value())
+            value |= mask;
+        mask >>= 1;
+        GPIO_ADC_IOCLK.set_value(0);
+        usleep(us);
     }
     usleep(us);
     GPIO_ADC_CS.set_value(1);
     return value;
 }
 
-void AlphaBot::timerEvent() {
+void AlphaBot::timerEvent()
+{
     // battery
     batteryLevel = ((float)readADC(ADC_VIN)) / ((float)ADCmax) * ((float)ADCvref) * 2.0;
     // callback
     if (nullptr != batteryCallback)
-	batteryCallback->hasBatteryVoltage(batteryLevel);
+        batteryCallback->hasBatteryVoltage(batteryLevel);
 }
 
-void AlphaBot::setRightWheelSpeed(float speed) {
-	speed = -speed;
-        if (speed < -1)
-                speed = -1;
-        if (speed > 1)
-                speed = 1;
-        rightWheelSpeed = speed;
-	right_wheel_pwm.setDutyCycleNanosecs(speed2nanosec(speed));
+void AlphaBot::setRightWheelSpeed(float speed)
+{
+    speed = -speed;
+    if (speed < -1)
+        speed = -1;
+    if (speed > 1)
+        speed = 1;
+    rightWheelSpeed = speed;
+    right_wheel_pwm.setDutyCycleNanosecs(speed2nanosec(speed));
 }
 
-void AlphaBot::setLeftWheelSpeed(float speed) {
-        if (speed < -1)
-                speed = -1;
-        if (speed > 1)
-                speed = 1;
-        leftWheelSpeed = speed;
-	left_wheel_pwm.setDutyCycleNanosecs(speed2nanosec(speed));
+void AlphaBot::setLeftWheelSpeed(float speed)
+{
+    if (speed < -1)
+        speed = -1;
+    if (speed > 1)
+        speed = 1;
+    leftWheelSpeed = speed;
+    left_wheel_pwm.setDutyCycleNanosecs(speed2nanosec(speed));
 }
--- alphabot.h
+++ alphabot.h
@@ -15,54 +15,54 @@
 
 #define DEFAULT_SAMPLING_INTERVAL_MS 500 // ms
 
-struct GPIO {
-    GPIO(int pinNo,
-	 bool isOutput = false)
+struct GPIO
+{
+    GPIO(int GPIOpinNo,
+         gpiod::line::direction direction)
     {
-	
-	const int chipNo = 0;
-	const std::string chipPath = std::format("/dev/gpiochip{}", chipNo);
-	const std::string consumername = std::format("gpioconsumer_{}_{}", chipNo, pinNo);
-	
-	// Config the pin as input and detecting falling and rising edegs
-	gpiod::line_config line_cfg;
-	if (isOutput) {
-	    line_cfg.add_line_settings(
-				       pinNo,
-				       gpiod::line_settings()
-				       .set_direction(gpiod::line::direction::OUTPUT)
-				       );
-	} else {
-	    line_cfg.add_line_settings(
-				       pinNo,
-				       gpiod::line_settings()
-				       .set_direction(gpiod::line::direction::INPUT)
-				       );
-	}
-	chip = std::make_shared<gpiod::chip>(chipPath);
-	auto builder = chip->prepare_request();
-	builder.set_consumer(consumername);
-	builder.set_line_config(line_cfg);
-	request = std::make_shared<gpiod::line_request>(builder.do_request());
-	if (!request) {
-	    std::cerr << consumername << " could not be opened.\n";
-	    exit(1);
-	}
+        pinNo = GPIOpinNo;
+        const int chipNo = 0;
+        const std::string chipPath = std::format("/dev/gpiochip{}", chipNo);
+        const std::string consumername = std::format("gpioconsumer_{}_{}", chipNo, pinNo);
+
+        // Config the pin as input and detecting falling and rising edegs
+        gpiod::line_config line_cfg;
+        line_cfg.add_line_settings(
+            GPIOpinNo,
+            gpiod::line_settings().set_direction(direction));
+        chip = std::make_shared<gpiod::chip>(chipPath);
+        auto builder = chip->prepare_request();
+        builder.set_consumer(consumername);
+        builder.set_line_config(line_cfg);
+        request = std::make_shared<gpiod::line_request>(builder.do_request());
+        if (!request)
+        {
+            std::cerr << consumername << " could not be opened.\n";
+            exit(1);
+        }
     }
-    
-    int get_value() {
-	return (int)(request->get_value(0));
+
+    bool get_value()
+    {
+        return gpiod::line::value::ACTIVE == (request->get_value(pinNo));
     }
-    
-    void set_value(int v) {
-	request->set_value(0,(gpiod::line::value)v);
+
+    void set_value(bool v)
+    {
+        if (v)
+        {
+            request->set_value(pinNo, gpiod::line::value::ACTIVE);
+        }
+        else
+        {
+            request->set_value(pinNo, gpiod::line::value::INACTIVE);
+        }
     }
-    
+
     std::shared_ptr<gpiod::chip> chip;
     std::shared_ptr<gpiod::line_request> request;
+    int pinNo;
 };
-
-
 
 /**
  * Alphabot class which communicates with the Alphabot hardware
@@ -75,45 +75,46 @@ public:
      * rate. The method step needs to be implemented.
      */
     class BatteryCallback
-        {
-        public:
-	    /**
-	     * Called at the specified sampling rate with a reference
-	     * to the Alphabot instance which has all the getters for
-	     * the data and setters for the motor control.
-	     */
-	    virtual void hasBatteryVoltage(float volts) = 0;
-        };
+    {
+    public:
+        /**
+         * Called at the specified sampling rate with a reference
+         * to the Alphabot instance which has all the getters for
+         * the data and setters for the motor control.
+         */
+        virtual void hasBatteryVoltage(float volts) = 0;
+    };
 
 public:
     /**
      * Starts the callbacks about the battery level and enables the GPIO/PWM.
-     * 
+     *
      * @param _samplingInterval Sampling interval in ms for the ADC data
      */
     void start(long _samplingInterval = DEFAULT_SAMPLING_INTERVAL_MS);
-    
+
     /**
      * Stops the callbacks about the battery level and shuts down the GPIO/PWM.
      */
     void stop();
-    
+
     /**
      * Destroys the Alpha Bot object and stops any communication
      */
-    ~AlphaBot() {
-	stop();
+    ~AlphaBot()
+    {
+        stop();
     }
-    
-    /** 
+
+    /**
      * Registers callback which signals a battery voltage reading
      * @param _batterycallback A pointer to the callback interface
      **/
     void registerBatteryCallback(BatteryCallback *_batterycallback)
-        {
-	    batteryCallback = _batterycallback;
-        }
-    
+    {
+        batteryCallback = _batterycallback;
+    }
+
     /**
      * Set the Left Wheel Speed
      * @param speed between -1 and +1
@@ -135,29 +136,30 @@ private:
     static constexpr float motor_rest_pulseduration_ns = 1.5 * 1E6;
     static constexpr float speed2pwm_ns = 0.1 * 1E6; // maps +-1 to +-0.1ms
 
-    int speed2nanosec(float speed) const {
-	return motor_rest_pulseduration_ns + speed * speed2pwm_ns;
+    int speed2nanosec(float speed) const
+    {
+        return motor_rest_pulseduration_ns + speed * speed2pwm_ns;
     }
-    
+
     // TLC1543
-    GPIO GPIO_ADC_IOCLK{25,true};
-    GPIO GPIO_ADC_ADDR{24,true};
-    GPIO GPIO_ADC_DOUT{23,false};
-    GPIO GPIO_ADC_CS{5,true};
+    GPIO GPIO_ADC_IOCLK{25, gpiod::line::direction::OUTPUT};
+    GPIO GPIO_ADC_ADDR{24, gpiod::line::direction::OUTPUT};
+    GPIO GPIO_ADC_DOUT{23, gpiod::line::direction::INPUT};
+    GPIO GPIO_ADC_CS{5, gpiod::line::direction::OUTPUT};
     static constexpr float ADCmax = 1023;
     static constexpr float ADCvref = 5;
-    
+
     void timerEvent();
-    
+
     unsigned readADC(unsigned ch);
-    
+
     long samplingInterval;
     BatteryCallback *batteryCallback = nullptr;
     float leftWheelSpeed = 0;
     float rightWheelSpeed = 0;
     float batteryLevel = 0;
     bool running = false;
-    
+
     RPI_PWM left_wheel_pwm;
     RPI_PWM right_wheel_pwm;
 };
